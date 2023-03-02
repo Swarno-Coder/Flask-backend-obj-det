df54077577f997d1b07f9fa7dff219e426b3bfdc
swarno-coder <swarnodipnag2004@gmail.com>
2023-03-02 23:41:58 +0530

    unnecessary lines cleanup

--- pythonn/server.py
+++ pythonn/server.py
@@ -12,14 +12,7 @@ def send_img():
     img = request.files['image']
     img = Image.open(img.stream)
     thres = 0.55 # Threshold to detect object
-    '''
-    if webcam:
-        cap = cv2.VideoCapture(1)
-        cap.set(3,1280)
-        cap.set(4,720)
-        cap.set(10,70)
-    '''
-
+    
     classNames= []
     classFile = '..\\coco.names'
     with open(classFile,'rt') as f:
@@ -44,30 +37,9 @@ def send_img():
                 cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
             cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
                 cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-
-    '''
-    while True:
-        if webcam:
-            _ ,img = cap.read()
-            img = cv2.resize(img,(0,0),None,0.5,0.5)
-            img = cv2.flip(img,1)
-        else: img = cv2.imread("..\\test\\2.jpg")
-        classIds, confs, bbox = net.detect(img,confThreshold=thres)
-        print(classIds,bbox)
-
-        if len(classIds) != 0:
-            for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-                cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-
-        cv2.imshow('Result',cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
-    '''
-
+            
+    return request.post()
     
+if __name__ == "__main__":
+    app.run(debug=True)
 
